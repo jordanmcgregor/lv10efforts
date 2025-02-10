282c4c5b6f5499c8202b899e842595bf2b031cc6
jmmcgregor <jordanmmcgregor@gmail.com>
2025-02-10 10:24:11 -0700

    added in home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,39 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 export default async function Home() {
   return (
     <>
-      <Hero />
+      {/* <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      </main> */}
+      <div className="p-16">
+        <span className="underline">Jeff's Organizations</span>
+        <ul className="list-disc">
+          <li>
+            <a className="text-blue-500 underline hover:text-blue-700" href="/organization/relief-society/efforts">Relief Society</a>
+          </li>
+          <li>
+            <a className="text-blue-500 underline hover:text-blue-700" href="/organization/primary/efforts">Primary</a>
+          </li>
+          <li>
+            <a className="text-blue-500 underline hover:text-blue-700" href="/organization/temple-family-history/efforts">Temple & Family History</a>
+          </li>
+        </ul>
+      </div>
+      <div className="p-16">
+        <span className="underline">Jordan's Organizations</span>
+        <ul className="list-disc">
+          <li>
+            <a className="text-blue-500 underline hover:text-blue-700" href="/organization/elders-quorum/efforts">Elders Quorum</a>
+          </li>
+          <li>
+            <a className="text-blue-500 underline hover:text-blue-700" href="/organization/missionary/efforts">Missionary</a>
+          </li>
+          <li>
+            <a className="text-blue-500 underline hover:text-blue-700" href="/organization/sunday-school/efforts">Sunday School</a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
